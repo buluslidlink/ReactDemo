b3ea63d48a003eb52126f007e03d41a2f093e146
buluslidlink <hailong.li@cn.dlink.com>
2016-12-19 18:36:48 +0800

    react-router

--- app.js
+++ app.js
@@ -1,31 +1,54 @@
 //require(`webpack-dev-server/bin/webpack-dev-server.js`);
 
-var path = require(`path`);
-var express = require(`express`);
-var webpack = require(`webpack`);
-var config = require(`./webpack.config`);
-var compiler = webpack(config);
+// var path = require(`path`);
+// var express = require(`express`);
+// var webpack = require(`webpack`);
+// var config = require(`./webpack.config`);
+// var compiler = webpack(config);
+// var app = express();
+//
+// var webpackDevOptions = {
+//     historyApiFallback: true,//browserHistory时，显示正常的路径example.com/some/path，背后调用的是浏览器的History API。但是，这种情况需要对服务器改造。否则用户直接向服务器请求某个子路由，会显示网页找不到的404错误。
+//     publicPath: config.output.publicPath,
+//     header: {
+//         'Access-Control-Allow-Origin': '*'
+//     }
+// };
+//
+// app.use(`/svr`, function (req, res) {
+//     res.send(`success`);
+// });
+// //app.use(express.static(path.resolve(__dirname,`js`)));
+// app.use(require(`webpack-dev-middleware`)(compiler, webpackDevOptions));
+// app.use(require(`webpack-hot-middleware`)(compiler));
+//
+// app.use(`*`, function (req, res) {
+//     res.sendFile(path.join(__dirname, `index.html`));
+// });
+//
+// app.listen(8787, `0.0.0.0`, function (err) {
+//     if (err) {
+//         console.log(err);
+//     }
+// });
+
+var express = require('express');
+var path = require('path');
+var compression = require('compression');
+
 var app = express();
 
-var webpackDevOptions = {
-    publicPath: config.output.publicPath
-};
+app.use(compression());
 
-app.use(`/svr`, function (req, res) {
-    res.send(`success`);
-});
-//app.use(express.static(path.resolve(__dirname,`js`)));
-app.use(require(`webpack-dev-middleware`)(compiler, webpackDevOptions));
-app.use(require(`webpack-hot-middleware`)(compiler));
+// serve our static stuff like index.css
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.use(`/test`, function (req, res) {
-    res.sendFile(path.join(__dirname, `index.html`));
-});
+// send all requests to index.html so browserHistory works
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'))
+})
 
-app.listen(8787, `0.0.0.0`, function (err) {
-    if (err) {
-        console.log(err);
-    }
-});
-
-console.log(process.env.NODE_ENV);
+var PORT = process.env.PORT || 8080
+app.listen(PORT, function () {
+    console.log('Production Express server running at localhost:' + PORT)
+})
--- webpack.config.js
+++ webpack.config.js
@@ -13,16 +13,18 @@
 var webpack = require('webpack');
 var path = require('path');
 var etp = require('extract-text-webpack-plugin');
+var is_env_dev = (process.env.NODE_ENV);
+console.log('is_env_dev:', is_env_dev);
 module.exports = {
     entry: [
-        'webpack-hot-middleware/client', //這隻檔案會連到 server 目的是當 server 重新編譯好檔案時收到通知然後更新 client 的檔案。
-        // 'webpack-dev-server/client?http://0.0.0.0:8080', /*webpack-dev-server运行时，通过该脚本与资源服务器通信完成刷新，http参数表示可以跨域访问资源*/
-        './js/test1.js', //必须这样写，不能写成js/test.js
-        'jquery'
+        //這隻檔案會連到 server 目的是當 server 重新編譯好檔案時收到通知然後更新 client 的檔案。
+        //'webpack-dev-server/client?http://0.0.0.0:8080', /*webpack-dev-server运行时，通过该脚本与资源服务器通信完成刷新，http参数表示可以跨域访问资源*/
+        './js/react/test.js', //必须这样写，不能写成js/test.js
+        // 'jquery'
     ],
     output: {
-        path: '/build',
-        publicPath: '/file',
+        path: 'build',
+        publicPath: '/build',
         name: 'bundle.js'
     },
     resolve: {
@@ -33,7 +35,7 @@ module.exports = {
             test: /\.jsx?$/,
             include: [path.resolve(__dirname, 'js')],
             // exclude: /node_modules/,
-            loaders: ['react-hot', 'babel']
+            loaders: ['babel']
         },
             {
                 test: /\.(css|less)$/,
@@ -44,21 +46,9 @@ module.exports = {
         ]
     },
     plugins: [
-        //  new ExtractTextPlugin("style.css", {allChunks: true})
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.optimize.CommonsChunkPlugin('jquery', 'jquery.bundle.js'),
-        new webpack.DefinePlugin({ //定义环境变量
-            debug: !!process.env.dev,
-            release: !!process.env.release
-        }),
-        new webpack.ProvidePlugin({ //定义别名，项目中直接引用，不需要import或者require
-            //  'Moment': 'moment',
-            "$": "jquery",
-            "jQuery": "jquery",
-            "window.jQuery": "jquery",
-            "React": "react"
-        }),
-        new webpack.NoErrorsPlugin()
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        // new webpack.optimize.UglifyJsPlugin()
     ]
 }
 
